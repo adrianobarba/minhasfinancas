6c107a9218e35460ffcb5e48ef146f7ad95b1c3e
adrianobarba <adrianobarbosa_4@hotmail.com>
2022-03-15 14:45:18 -0300

    projeto inicial

---
+++ src/main/java/com/adriano/minhasfinancas/model/entity/StatusLancamento.java
@@ -0,0 +1,10 @@
+package com.adriano.minhasfinancas.model.entity;
+
+public enum StatusLancamento {
+	
+	PENDENTE,
+	CANCELADO,
+	EFETIVADO
+
+
+}
---
+++ src/main/java/com/adriano/minhasfinancas/model/entity/TipoLancamento.java
@@ -0,0 +1,7 @@
+package com.adriano.minhasfinancas.model.entity;
+
+public enum TipoLancamento {
+
+	RECEITA,
+	DESPESA
+}
